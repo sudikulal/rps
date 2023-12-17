0977db7f4e678921242fe2c204ee47a243af2bbd
Sudikulal <sudikulal325@gmail.com>
2023-12-17 10:39:27 +0530

    Update:updated image

--- client/src/assets/Images.js
+++ client/src/assets/Images.js
@@ -1,11 +1,12 @@
+import rock from "./rock.png";
+import scissor from "./scissor.png";
+import paper from "./paper.png";
+import pending from "./pending.png";
 const image = {
-  pending:
-    "https://w7.pngwing.com/pngs/334/751/png-transparent-rock-paper-scissors-hand-game-scissors-game-text-technic-thumbnail.png",
-  rock: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2s5agiffO_2ojK_TpgUZCJppVFTzLIQEdg&usqp=CAU",
-  paper:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTefSEcpEACwShuHU1j58RFe-JY0LXP1MxvgQ&usqp=CAU",
-  scissor:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG5wJISO76_DRX1WlQACtr0EMJYiliLMS5rw&usqp=CAU",
+  pending,
+  rock,
+  paper,
+  scissor,
 };
 
-export {image}
+export { image };
