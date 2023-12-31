d5f2f586088e6cbcc1587eb035f128522f0a8d07
Sudikulal <sudikulal325@gmail.com>
2023-12-31 20:38:05 +0530

    Fix:cross origin request block

--- server/index.js
+++ server/index.js
@@ -4,16 +4,21 @@ const socketIO = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "*" }));
+app.use(cors());
 
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.get("/", (req, res) => res.send("hello world"));
 
 require("./game.controller.js")(io);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
